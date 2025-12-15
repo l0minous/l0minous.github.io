7400655a183ecb9fb0b245b79098f314b6b030c6
l0minous <zazadiler7@gmail.com>
2025-12-15 01:28:19 -0500

    chore: configure for github pages deployment and fix assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@
   import path from 'path';
 
   export default defineConfig({
-    plugins: [react()],
-    resolve: {
+    base: '/',
+    plugins: [react()],    resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
         'vaul@1.1.2': 'vaul',
